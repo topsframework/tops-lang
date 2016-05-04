f0ac25e3b906422ed8311c187f71454f57a4b407
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2016-05-03 23:47:31 -0300

    Remove './' and '//' from returned value of path manipulation functions

--- src/lang.cpp
+++ src/lang.cpp
@@ -25,6 +25,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <iomanip>
 #include <fstream>
 #include <sstream>
 #include <utility>
@@ -768,23 +769,38 @@ using is_pair = detail::is_pair_impl<T>;
 
 // Path manipulation
 
+std::string removeSubstring(const std::string &substr, std::string str) {
+  if (substr.empty()) return str;
+
+  auto it = str.find(substr);
+  while (it != std::string::npos) {
+    str.erase(it, substr.length());
+    it = str.find(substr);
+  }
+  return str;
+}
+
+std::string cleanPath(std::string filepath) {
+  return removeSubstring("//", removeSubstring("./", filepath));
+}
+
 std::string extractRoot(const std::string &filepath) {
   auto found = filepath.find_first_of("/\\");
-  return filepath.substr(0, found + 1);
+  return cleanPath(filepath).substr(0, found + 1);
 }
 
 std::string extractCorename(const std::string &filepath) {
   auto found = filepath.find_first_of("/\\");
-  return filepath.substr(found + 1);
+  return cleanPath(filepath).substr(found + 1);
 }
 
 std::string extractDir(const std::string &filepath) {
   auto found = filepath.find_last_of("/\\");
-  return filepath.substr(0, found + 1);
+  return cleanPath(filepath).substr(0, found + 1);
 }
 
 std::string extractBasename(const std::string &filepath) {
-  auto found = filepath.find_last_of("/\\");
+  auto found = cleanPath(filepath).find_last_of("/\\");
   return filepath.substr(found + 1);
 }
 
